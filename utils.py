9b21c3756a84350db39a9b7741c185e7b4ab1179
Magnus Kirø <magnuskiro@gmail.com>
2015-03-23 17:36:39 +0100

    comments

--- utils.py
+++ utils.py
@@ -1,13 +1,19 @@
 import math
 
+'''
+Generates prime number under a given higher limit.
+@param lim: the higher limit of wanted primes. 
+'''
 def getPrimes(lim):
     primes = []
+    # append the first prime
     primes.append(2)
     #print "primes: ", primes
+    # for all numbers below the higher limit. 
     for i in range(3,lim+1):
         isPrime = True
         #print "num!",i
-        # check the number against the already registered primes. 
+        # check the current number against the already registered primes. 
         for p in primes:
             #print "--", i, p
             # if the current modulo prime == 0, it means that current is not a
